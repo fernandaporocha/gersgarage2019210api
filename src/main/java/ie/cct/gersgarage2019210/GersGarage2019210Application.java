2d01eada178f36b28518d11548da346cb699e645
Fernanda Rocha <fernandaporocha@gmail.com>
2020-06-24 01:20:11 +0100

    Starting the Spring Project

---
+++ src/main/java/ie/cct/gersgarage2019210/GersGarage2019210Application.java
@@ -0,0 +1,13 @@
+package ie.cct.gersgarage2019210;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GersGarage2019210Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GersGarage2019210Application.class, args);
+	}
+
+}
---
+++ src/test/java/ie/cct/gersgarage2019210/GersGarage2019210ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.cct.gersgarage2019210;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GersGarage2019210ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
