4fee1cd5f90cb5d71cc65aa4b64b48446b13b979
Fernanda Rocha <fernandaporocha@gmail.com>
2020-08-05 17:19:37 +0100

    Adding price and doubledSlot fields

--- src/main/java/ie/cct/gersgarage2019210/model/BookingType.java
+++ src/main/java/ie/cct/gersgarage2019210/model/BookingType.java
@@ -1,5 +1,7 @@
 package ie.cct.gersgarage2019210.model;
 
+import java.math.BigDecimal;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,4 +20,6 @@ public class BookingType {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	private BigDecimal price;
+	private boolean doubledSlot;
 }
