38bcf3246336799babb9184a67cd207d2ef3a8ce
Fernanda Rocha <fernandaporocha@gmail.com>
2020-08-16 14:13:38 +0100

    Changing some HQLs

--- src/main/java/ie/cct/gersgarage2019210/repository/BookingRepository.java
+++ src/main/java/ie/cct/gersgarage2019210/repository/BookingRepository.java
@@ -7,14 +7,20 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import ie.cct.gersgarage2019210.model.Booking;
+import ie.cct.gersgarage2019210.model.User;
 
 @Repository
 public interface BookingRepository extends CrudRepository<Booking, Integer>{
 	
-	List<Booking> findByCustomer(Integer customerId);
+	List<Booking> findByCustomer(User customerId);
 	
-	List<Booking> findByBookingDate(LocalDate date);
+	List<Booking> findByResponsibleStaff(User mechanic);
+	
+	List<Booking> findByDate(LocalDate date);
 	
 	//https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
-	List<Booking> findByBookingDateBetween (LocalDate minDate, LocalDate maxDate);
+	List<Booking> findByDateBetween (LocalDate minDate, LocalDate maxDate);
+	
+	List<Booking> findByResponsibleStaffAndDate(User mechanic, LocalDate date);
+	
 }
